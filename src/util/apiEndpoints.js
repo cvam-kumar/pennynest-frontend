b1c245ed085eaa1fe159b556655ec200185c2b2d
Shivam Kumar <shivam10800114@gmail.com>
2025-08-26 10:03:30 +0530

    connected to deployed backend server

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://penny-nest-api.onrender.com/api/v1.0";
-export const BASE_URL = "http://localhost:8080/api/v1.0";
+export const BASE_URL = "https://penny-nest-api.onrender.com/api/v1.0";
+//export const BASE_URL = "http://localhost:8080/api/v1.0";
 
 const CLOUDINARY_CLOUD_NAME = "dceo07xez";
 
